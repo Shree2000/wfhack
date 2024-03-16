d2a92ae5355af9bafcc4366111246100848206c9
Shivesh Dixit <dixitshivesh55@gmail.com>
2024-03-16 11:54:16 +0530

    training for weight

--- code/ProcessAudio/NeuralNetwork.py
+++ code/ProcessAudio/NeuralNetwork.py
@@ -1,13 +1,14 @@
 from sklearn.externals import joblib
 import os
+import numpy as np
 # Get the directory where the current Python script is located
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 def process(audio_data):
-    # Call different models with audio data and obtain their predictions
+    #Call different models with audio data and obtain their predictions
     model1_predictions = call_model1(audio_data)
     model2_predictions = call_model2(audio_data)
-    # Add more model calls as needed
+    #Add more model calls as needed
 
     # Combine predictions into a single feature vector
     combined_features = np.concatenate((model1_predictions, model2_predictions), axis=0)
